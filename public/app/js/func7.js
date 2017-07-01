f2fcf403552669cb8723ce5ea7267b1ccb62f83d
Zebar01 <pvm_moroz@mail.ru>
2017-07-01 15:44:33 +0300

    Update func7.js

--- public/app/js/func7.js
+++ public/app/js/func7.js
@@ -12,66 +12,33 @@ let den = now.getDate();
 //***************************************************************************************************************************************************************** */
 // проверка существования json за неделю
 let mas_maxpr=[]        //массив с максимальными ценами
-let avg_maxpr=[]        //массив с максимальными ценами
-let min_maxpr=[]        //массив с максимальными ценами
+let mas_avgpr=[]        //массив с максимальными ценами
+let mas_minpr=[]        //массив с максимальными ценами
 
-for (let i=0; i<1; i++){
-      now.setDate(now.getDate()-1);
-      //console.log(now);
-      god = now.getFullYear();
-      mes = now.getMonth()+1;
-      den = now.getDate();
-      let d2 = god+'-'+mes+'-'+den;
+
+      for (let i=0; i<7; i++){
+            now.setDate(now.getDate()-1);
+            //console.log(now);
+            god = now.getFullYear();
+            mes = now.getMonth()+1;
+            den = now.getDate();
+            let d2 = god+'-'+mes+'-'+den;
+            
+
+            //let pathjson=ch2+'public\\json\\cian\\'+god+'\\'+mes+'\\'+den+'\\'+'lite2\\'+'33-1-lite.json';
+            let pathjson = '/json/myfile.json';
+
+            //async calls
+            $.get(pathjson)
+            .then(function(data){
+                  mas_maxpr[i]=data[0].maxpr;
+                  mas_avgpr[i]=data[0].avgpr;
+                  mas_minpr[i]=data[0].minpr;
+                  if ((mas_maxpr.length==7) && (mas_avgpr.length==7) && (mas_minpr.length==7)){
+                        alert(mas_maxpr[6]+', '+mas_avgpr[6]+', '+mas_minpr[6]);
+
+                  }
+            });      
       
-
-      //let pathjson=ch2+'public\\json\\cian\\'+god+'\\'+mes+'\\'+den+'\\'+'lite2\\'+'33-1-lite.json';
-      let pathjson = '/json/myfile.json';
-
-      //async calls
-      $.get(pathjson)
-      .then(function(data){
-            mas_maxpr[i]=data[0].maxpr;
-            avg_maxpr[i]=data[0].avgpr;
-            min_maxpr[i]=data[0].minpr;
-      });      
-};
-
-//console.log(obj[0].avgpr);
-
-/*
-      
-
-
-//}
-/*now = new Date();
-god = now.getFullYear();
-mes = now.getMonth();
-den = now.getDate()-1;
-let d2=god+'-'+mes+'-'+den;
-//console.log(d2);
-d2 = new Date(god,mes,den);
-console.log(d2);
-//alert(d2);
-let d3=[];
-d3[0]=d2;
-for (let i=0; i<7; i++){
-
-      d3[i]=d2.setUTCDate(den - i);
-      let god2=d2.getFullYear();
-      let mes2=d2.getMonth();
-      let den2=d2.getDate();
-      d3[i]=god2+'-'+mes2+'-'+den2;
-}
-//alert(d2);
-console.log(d3);
-/*for (let i=0; i<d3.length; i++){
-      let tmpdate=split('-',3);
-      let tmpy=tmpdate[0];
-      let tmpm=tmpdate[1];
-      let tmpd=tmpdate[2];
-      let filename= tmpy+'-'+tmpm+'-'+tmpd;
-}
-
-
-*/
-}
+      };
+} 
